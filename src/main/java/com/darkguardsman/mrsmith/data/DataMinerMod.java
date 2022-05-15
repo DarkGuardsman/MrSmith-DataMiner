71dcaf8ea6647c90a360c34a972ab3d2fd164e42
Robin Seifert <rseifert.phone@gmail.com>
2022-05-15 03:10:00 -0400

    feat: implemented item registry export with subtype handling

--- src/main/java/com/darkguardsman/mrsmith/data/DataMinerMod.java
+++ src/main/java/com/darkguardsman/mrsmith/data/DataMinerMod.java
@@ -1,6 +1,13 @@
 package com.darkguardsman.mrsmith.data;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -11,30 +18,36 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
 @Mod(modid = DataMinerMod.ID, name = "MrSmith - Data Miner", version = "0.1.0")
-public final class DataMinerMod
-{
+public final class DataMinerMod {
+
+    private static final Logger LOG = LogManager.getLogger();
     public static final String ID = "mrsmith-dataminer";
 
     @Mod.Instance(DataMinerMod.ID)
     public static DataMinerMod INSTANCE;
 
     @Mod.EventHandler
-    public void loadComplete(FMLLoadCompleteEvent event)
-    {
+    public void loadComplete(FMLLoadCompleteEvent event) {
         dumpItemRegistry();
         FMLCommonHandler.instance().exitJava(0, false);
     }
 
     private void dumpItemRegistry() {
 
+        final Gson gson = new Gson();
+
         final File saveFolder = new File("./MrSmith/exports/items");
-        if(!saveFolder.exists()) {
+        if (!saveFolder.exists()) {
             saveFolder.mkdirs();
         }
 
@@ -48,7 +61,7 @@ public final class DataMinerMod
 
             //Make domain folder if it doesn't exist
             final File domainFolder = new File(saveFolder, "/" + domain);
-            if(!domainFolder.exists()) {
+            if (!domainFolder.exists()) {
                 domainFolder.mkdirs();
             }
 
@@ -57,10 +70,58 @@ public final class DataMinerMod
             //Log file we saved again for fast lookup later
             resourceLocationToFile.put(item.getRegistryName().toString(), saveFile.getPath());
 
-            //TODO write data
+            final JsonObject fileEntry = new JsonObject();
+            fileEntry.addProperty("key", item.getRegistryName().toString());
+            fileEntry.addProperty("name", item.getItemStackDisplayName(item.getDefaultInstance()));
+            fileEntry.addProperty("localization", item.getUnlocalizedName(item.getDefaultInstance()));
+
+            writeJson(gson, fileEntry, saveFile);
+
+
+            //Handle subtypes
+            if(item.getHasSubtypes()) {
+                final NonNullList<ItemStack> items = NonNullList.create(); //TODO handle duplicates, NBT data, etc
+                for(CreativeTabs tab : item.getCreativeTabs()) {
+                    item.getSubItems(tab, items);
+                }
+
+                items.forEach(subItem -> {
+                    final String subTypeKey = item.getRegistryName().toString() + "@" + subItem.getItemDamage();
+                    final File subTypeSaveFile = new File(domainFolder, "/" + subTypeKey.replace("@", "_")  + ".json"); //TODO lint file name
+
+                    //Log file we saved again for fast lookup later
+                    resourceLocationToFile.put(subTypeKey, subTypeSaveFile.getPath());
+
+                    final JsonObject subTypeEntry = new JsonObject();
+                    subTypeEntry.addProperty("key", subTypeKey);
+                    subTypeEntry.addProperty("parent", item.getRegistryName().toString());
+                    subTypeEntry.addProperty("name", item.getItemStackDisplayName(subItem));
+                    subTypeEntry.addProperty("localization", item.getUnlocalizedName(subItem));
+
+                    writeJson(gson, subTypeEntry, subTypeSaveFile);
+                });
+            }
 
         });
 
+        final JsonArray fileList = new JsonArray();
 
+        resourceLocationToFile.forEach((key, value) -> {
+            final JsonObject fileEntry = new JsonObject();
+            fileEntry.addProperty("key", key);
+            fileEntry.addProperty("file", value);
+            fileList.add(fileEntry);
+        });
+
+        final File itemsFile = new File(saveFolder, "items.json");
+        writeJson(gson, fileList, itemsFile);
+    }
+
+    private void writeJson(final Gson gson, final JsonElement writeData, final File file) {
+        try (Writer writer = new FileWriter(file)) {
+            gson.toJson(writeData, writer);
+        } catch (Exception e) {
+            LOG.error("Failed to write file: " + file, e);
+        }
     }
 }
